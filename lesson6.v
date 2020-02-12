762182d8768e7a181c11f77c1b5edba6a09b2741
Assia Mahboubi <Assia.Mahboubi@inria.fr>
2020-02-12 18:13:32 +0100

    small progress on lesson 6 typesetting

--- lesson6.v
+++ lesson6.v
@@ -5,7 +5,101 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 (**
 ----------------------------------------------------------
-#<div class="slide">#
+----------------------------------------------------------
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Type-inference Based Automation
+
+Today:
+
+- Automating the synthesis of statements
+- Automating proofs by enhanced unification
+- Mathematical structures in dependent type theory
+
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Redundant annotations
+
+(polymorphic lists)
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Matching and unification
+
+(apply and rewrite)
+
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Coercions
+
+(so far only from the context, now from the user)
+
+bool -> Prop
+
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Coercions
+
+(so far only from the context, now from the user)
+
+First projection of a dependent pair (val).
+
+
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Reminder: currification
+
+
+
+#<div>#
+*)
+Variables A1 B1 C1 : Type.
+Check A1 -> B1 -> C1.
+Check A1 * B1 -> C1.
+
+
+Variables A2 B2 C2 : Prop.
+Check A2 -> B2 -> C2.
+Check A2 /\ B2 -> C2.
+
+
+
+(**
+#</div>#
+
+#</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** Currification, the dependent case
+
+
+#<div>#
+*)
+Record posnat : Type := Posnat {nval : nat; nvalP : 0 < nval}.
+
+Variable P : nat -> Prop.
+
+Check forall n : nat, 0 < n -> P n.
+Check forall x : posnat, P (nval x).
+
+(**
+#</div>#
+
+#</div>#
+
+----------------------------------------------------------
+#<div class="slide vfill">#
 
 ** Organizing a library and teaching its contents to Coq
 
@@ -49,5 +143,13 @@ class, you just need a recent browser thanks to
 #</div></div>#
 
 #</div>#
+----------------------------------------------------------
+#<div class="slide vfill">#
+
+** This is another title
+
+
+#</div>#
+----------------------------------------------------------
 ----------------------------------------------------------
 *)
